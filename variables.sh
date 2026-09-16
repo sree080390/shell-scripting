41ed1f213eafc0cef8afb96117aea90eca2768c7
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-16 15:56:03 +0530

    new file variables.sh

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON=$1
+COMPANY=$2
+
+echo "$PERSON is joining $COMPANY in few days"
+echo "He is very excited to join $COMPANY and work on new projects"
+echo "He is also excited to meet new people and learn new things"
