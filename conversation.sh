9a81abd7c4b76f8dbab59cb9a581234aa27b598c
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-16 15:38:55 +0530

    conversation file added

---
+++ conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Srivatsasa is joining Apple in few days"
+echo "He is very excited to join Apple and work on new projects"
+echo "He is also excited to meet new people and learn new things"
