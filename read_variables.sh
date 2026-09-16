f003ecd9a5686bdf71bdf56efcb8f32dbd000032
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-16 17:09:40 +0530

    new file read_variables.sh

---
+++ read_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo  "Please enter your name: "
+read PERSON
+echo "$PERSON is joining the company in few days"
+echo "Please enter your password: "
+read -s PASSWORD
+echo "Password entered (for demonstration purposes only): $PASSWORD"
