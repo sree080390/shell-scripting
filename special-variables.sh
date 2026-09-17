55e905c523ce159ae864e48e7f6e1135400b100a
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-17 20:07:12 +0530

    new file special_variables.sh

--- special-variables.sh
+++ special-variables.sh
@@ -8,5 +8,5 @@ echo "Who is the current user: $USER"
 echo "In which directory the script is running: $PWD"
 echo "What is current user's home directory: $HOME"
 echo "What is the current process id: $$"
-sleep 5
+sleep 5 &
 echo "What is the last process id: $!"
