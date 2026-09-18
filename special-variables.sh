95e99624967b33798038f278c67135b94c9cb875
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-18 13:22:44 +0530

    new file special_variables.sh

--- special-variables.sh
+++ special-variables.sh
@@ -12,6 +12,7 @@ sleep 5 &
 echo "What is the last process id: $!"
 echo "PID of background process: wait $!"
 echo "What is the exit status of last command: $?"
-echo "What is the last command executed: $BASH_COMMAND"
 echo "what is the current shell: $SHELL"
 echo "what is line number of current command: $LINENO"
+echo "How much time the script has been running: $SECONDS"
+echo "Generate a random number: $RANDOM"
