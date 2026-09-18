38e29d704decb8cfa3aa48194bf448a271c0fd0c
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-18 14:20:16 +0530

    new file special_variables.sh

---
+++ arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MEMBERS=("Alice" "Bob" "Charlie" "David" "Eve")
+echo "All members: ${MEMBERS[@]}"
+echo "Number of members: ${#MEMBERS[@]}"    
+echo "First member: ${MEMBERS[0]}"
+echo "Last member: ${MEMBERS[-1]}"
+echo "All members in reverse order: ${MEMBERS[@]::-1}"
