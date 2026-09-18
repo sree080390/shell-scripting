47f553e2ddc4311c318aedf4c2599f1d76c46a57
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-18 14:25:26 +0530

    new file special_variables.sh

---
+++ conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter a number: " num
+if [ $num -gt 10 ]; then
+    echo "The number is greater than 10."
+elif [ $num -eq 10 ]; then
+    echo "The number is equal to 10."
+else
+    echo "The number is less than 10."
+fi
