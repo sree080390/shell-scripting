bf76ea2eb90bc51ee23937a35a51bd6404ced5b4
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-18 20:19:05 +0530

    new file special_variables.sh

---
+++ functions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo "You are not root user. Please run the script as root."
+    exit 1
+fi
+echo "You are root user. Proceeding with the script execution."
+
+SERVER_TO_BE_INSTALLED=$(read -p "Enter the server to be installed (e.g., httpd, nginx): " SERVER_TO_BE_INSTALLED)
+
+INSTALL($SERVER_TO_BE_INSTALLED)
+
+INSTALL () {
+    dnf list installed | grep -q $1
+if [ $? -eq 0 ]; then
+    echo "$1 is already installed."
+else
+    echo "$1 is not installed. Installing..."
+    dnf install -y $1
+fi
+
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install $1."
+    echo "script exit with status code 1"   exit 1
+else
+    echo "$1 installed successfully."
+    systemctl start $1
+    STATUS=$(systemctl status $1)
+    echo "$STATUS" | grep -q "active (running)"
+    if [ $? -eq 0 ]; then
+        echo "$1 is running."
+    else
+        echo "$1 failed to start."
+    fi
+fi
+
+}
+
