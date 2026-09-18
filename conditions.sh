b6d784a8712a258a78af7955b20a03897994c150
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-18 14:28:18 +0530

    new file special_variables.sh

--- conditions.sh
+++ conditions.sh
@@ -6,6 +6,7 @@ if [ $num -gt 10 ]; then
 elif [ $num -eq 10 ]; then
     echo "The number is equal to 10."
     exit 1 # If this condition is true, the script will exit with a status of 1
+    echo "exit processed"
 else
     echo "The number is less than 10."
 fi
