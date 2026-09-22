efad5d3f069ae621cf4ff914e3395f3377811231
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-22 22:49:45 +0530

    new file special_variables.sh

---
+++ set_trap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+USER_ID=$(id -u)
+INSTALL_LOGS_FILE="/var/log/shell-script.log"
+STATUS_LOGS_FILE="/var/log/shell-script-status.log"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+N="\e[0m"
+trap 'echo "Script execution interrupted at $(date +%Y-%m-%d_%H-%M-%S) and $LINENO, command: $BASH_COMMAND. Exiting..."' ERR
+#read -p "Enter the server to be installed (e.g., httpd, nginx): " SERVER_TO_BE_INSTALLED
+if [ $USER_ID -ne 0 ]; then
+    echo "Script execution started at $TIMESTAMP" | tee -a $STATUS_LOGS_FILE
+    echo -e "$TIMESTAMP [ERROR] You are $R not root user $N. Please run the script as root." | tee -a $STATUS_LOGS_FILE
+    exit 1
+fi
+echo -e "$TIMESTAMP [INFO] You are $G root user $N. Proceeding with the script execution."
+
+
+STATUS_VALIDATE () {
+    systemctl start $1 | tee -a $STATUS_LOGS_FILE
+    STATUS=$(systemctl status $1)
+    echo "$TIMESTAMP [INFO] $STATUS" | grep -q "active (running)" | tee -a $STATUS_LOGS_FILE
+    if [ $? -eq 0 ]; then
+        echo -e "$TIMESTAMP [INFO] $1 is $G running.$N" | tee -a $STATUS_LOGS_FILE
+    else
+        echo -e "$TIMESTAMP [ERROR] $1 $R failed to start.$N" | tee -a $STATUS_LOGS_FILE
+    fi
+}
+
+INSTALL () {
+    dnf list installed | grep -q $1
+if [ $? -eq 0 ]; then
+    echo -e "$TIMESTAMP [INFO] $1 is $Y already installed. $N" | tee -a $INSTALL_LOGS_FILE
+else
+    echo -e "$TIMESTAMP [ERROR] $1 is not $R installed. $N Installing..." | tee -a $INSTALL_LOGS_FILE
+    dnf install -y $1 &>> $INSTALL_LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $1 $R failed to install.$N" | tee -a $INSTALL_LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] script exit with status code 1" | tee -a $INSTALL_LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $1 $G installed successfully.$N" | tee -a $INSTALL_LOGS_FILE
+        if [ $1 == "mysql-server" ];then
+            STATUS_VALIDATE mysqld
+        else
+            STATUS_VALIDATE $1
+        fi
+    fi
+fi
+}
+
+for packages in $@
+do
+    INSTALL "$packages"
+done
+
