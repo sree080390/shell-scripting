49a2107737479a19c7f6f0c91d02b319668fd3f9
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-22 16:03:09 +0530

    new file special_variables.sh

--- loops.sh
+++ loops.sh
@@ -4,7 +4,7 @@ USER_ID=$(id -u)
 INSTALL_LOGS_FILE="/var/log/shell-script.log"
 STATUS_LOGS_FILE="/var/log/shell-script-status.log"
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-read -p "Enter the server to be installed (e.g., httpd, nginx): " SERVER_TO_BE_INSTALLED
+#read -p "Enter the server to be installed (e.g., httpd, nginx): " SERVER_TO_BE_INSTALLED
 if [ $USER_ID -ne 0 ]; then
     echo "Script execution started at $TIMESTAMP" | tee -a $STATUS_LOGS_FILE
     echo "$TIMESTAMP [ERROR] You are not root user. Please run the script as root." | tee -a $STATUS_LOGS_FILE
