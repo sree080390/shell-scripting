049370ea97a953d3a2ad2cfc39f6fbc818b59a5f
srivatsasa90 <srivatsasa90@gmail.com>
2026-09-22 15:00:32 +0530

    new file special_variables.sh

--- logs.sh
+++ logs.sh
@@ -11,28 +11,29 @@ fi
 echo "You are root user. Proceeding with the script execution."
 
 STATUS_VALIDATE () {
-    systemctl start $1
+    systemctl start $1 | tee -a $STATUS_LOGS_FILE
     STATUS=$(systemctl status $1)
-    echo "$STATUS" | grep -q "active (running)"
+    echo "$STATUS" | grep -q "active (running)" | tee -a $STATUS_LOGS_FILE
     if [ $? -eq 0 ]; then
-        echo "$1 is running."
+        echo "$1 is running." | tee -a $STATUS_LOGS_FILE
     else
-        echo "$1 failed to start."
+        echo "$1 failed to start." | tee -a $STATUS_LOGS_FILE
     fi
 }
 
 INSTALL () {
     dnf list installed | grep -q $1
 if [ $? -eq 0 ]; then
-    echo "$1 is already installed."
+    echo "$1 is already installed." | tee -a $INSTALL_LOGS_FILE
 else
     echo "$1 is not installed. Installing..."
     dnf install -y $1 &>> $INSTALL_LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "Failed to install $1."
-        echo "script exit with status code 1"   exit 1
+        echo "Failed to install $1." | tee -a $INSTALL_LOGS_FILE
+        echo "script exit with status code 1" | tee -a $INSTALL_LOGS_FILE
+        exit 1
     else
-        echo "$1 installed successfully."
+        echo "$1 installed successfully." | tee -a $INSTALL_LOGS_FILE
         if [ $1 == "mysql-server" ];then
             STATUS_VALIDATE mysqld
         else
